9ce3cac654d438c221cb58aa6911448fba619616
apex1000 <siddhartsingh9876@gmial.com>
2020-09-01 10:59:31 +0530

    CI: Build and push to username.github.io

--- deploy.sh
+++ deploy.sh
@@ -11,8 +11,8 @@ then
     echo $GITHUB_AUTH_SECRET > ~/.git-credentials
 
     git config credential.helper store
-    git config user.email "username-blog-bot@users.noreply.github.com"
-    git config user.name "username-blog-bot"
+    git config user.email "codewithsidy@gmail.com"
+    git config user.name "siddythings-bot"
 fi
 
 git add .
